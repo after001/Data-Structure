accbcc2d698bdf49aed6b308216c9a29a9ba896d
after001 <132841119+after001@users.noreply.github.com>
2023-09-21 13:45:25 -0300

    Update Recursividade.java

--- src/Recursividade.java
+++ src/Recursividade.java
@@ -1,10 +1,10 @@
 public class Recursividade {
-    
+
     public static int soma(int n) {
         if (n == 1) {
             return 1;
         } else {
-            return n + soma(n-1);
+            return n + soma(n - 1);
         }
     }
 
@@ -12,15 +12,15 @@ public class Recursividade {
         if (y == 0) {
             return 1;
         } else {
-            return x * exponencial(x, y-1);        
+            return x * exponencial(x, y - 1);
         }
     }
 
     public static void exibeVetor(int[] z, int length) {
         if (length != 0) {
-            System.out.println(z[length-1]);
-            exibeVetor(z, length-1);
-        } 
+            System.out.println(z[length - 1]);
+            exibeVetor(z, length - 1);
+        }
     }
 
     public static int somaVetor(int[] z, int length) {
@@ -34,22 +34,43 @@ public class Recursividade {
     public static void inverteVetor(int[] z, int n, int length) {
         if (n < length) {
             int aux = z[n];
-            z[n] = z[length -1];
+            z[n] = z[length - 1];
             z[length - 1] = aux;
             inverteVetor(z, n + 1, length - 1);
         }
     }
 
-    public static int retornaMenor(int[] z, int n, int length) {
-        int menor = 0;
-        
-        if (z[n] < z[length]) {
-            menor = z[n];
-        } else {
-            menor = z[length];
+    public static int retornaMenor(int[] z, int length) {
+        if (length == 1) {
+            return z[0];
         }
 
-        retornaMenor(z, n+1, length-1);
+        int menor = retornaMenor(z, length - 1);
+
+        if (menor < z[length - 1]) {
+            return menor;
+        } else {
+            return z[length - 1];
+        }
     }
 
+    public static int buscaBinaria(int[] v, int chave, int esq, int dir) {
+        if (esq > dir) {
+            return -1;
+        }
+
+        if (chave < v[esq] || chave > v[dir]) {
+            return -1;
+        }
+
+        int meio = (esq + dir) / 2;
+
+        if (v[meio] == chave) {
+            return meio;
+        } else if (v[meio] < chave) {
+            return buscaBinaria(v, chave, meio + 1, dir);
+        } else {
+            return buscaBinaria(v, chave, esq, meio - 1);
+        }
+    }
 }
